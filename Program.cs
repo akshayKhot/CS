00a2a7c16ee4e59e88518ccc654f815382256d39
akshaykhot <akshay.7277@gmail.com>
2020-07-01 21:38:38 -0700

    Add REPL support with basic Lexer and Parser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cc
 {
@@ -6,7 +7,191 @@ namespace cc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("> ");
+
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+
+                var lexer = new Lexer(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+
+                    Console.Write($"{token.Kind}: '{token.Text}'");
+
+                    if (token.Value != null)
+                        Console.Write($": {token.Value}");
+
+                    Console.WriteLine();
+                }
+            }
         }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+
+        public SyntaxKind Kind { get; }
+
+        public int Position { get; }
+
+        public string Text { get; }
+
+        public object Value { get; }
+    }
+
+    class Lexer
+    {
+        private readonly string _text;
+
+        private int _position;
+
+        private char Current
+        {
+            get
+            {
+                if (_position >= _text.Length)
+                    return '\0';
+
+                return _text[_position];
+            }
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+
+        public Lexer(string text)
+        {
+            _text = text;
+            _position = 0;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            if (_position >= _text.Length)
+            {
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+            }
+
+            if (char.IsDigit(Current))
+            {
+                int start = _position;
+
+                while (char.IsDigit(Current))
+                    Next();
+
+                int length = _position - start;
+                string text = _text.Substring(start, length);
+                int.TryParse(text, out int value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            if (char.IsWhiteSpace(Current))
+            {
+                int start = _position;
+
+                while (char.IsWhiteSpace(Current))
+                    Next();
+
+                int length = _position - start;
+                string text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
+            }
+
+            if (Current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+
+            if (Current == '-')
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+
+            if (Current == '*')
+                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+
+            if (Current == '/')
+                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+
+            if (Current == '(')
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+
+            if (Current == ')')
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+    }
+
+    abstract class SyntaxNode
+    {
+        public abstract SyntaxKind Kind { get; }
+    }
+
+    class Parser
+    {
+        private readonly SyntaxToken[] _tokens;
+
+        private int _position;
+
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
+            var lexer = new Lexer(text);
+            SyntaxToken token;
+
+            do
+            {
+                token = lexer.NextToken();
+
+                if (token.Kind != SyntaxKind.WhitespaceToken &&
+                    token.Kind != SyntaxKind.BadToken)
+                {
+                    tokens.Add(token);
+                }
+
+            } while (token.Kind != SyntaxKind.EndOfFileToken);
+
+            _tokens = tokens.ToArray();
+        }
+
+        private SyntaxToken Peek(int offset)
+        {
+            int index = _position + offset;
+
+            if (index >= _tokens.Length)
+                return _tokens[_tokens.Length - 1]; // return last token
+
+            return _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
     }
 }
