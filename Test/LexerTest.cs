e1ee4539e843a2dacea92847600dd8b19ab27d0e
akshaykhot <akshay.7277@gmail.com>
2020-07-03 19:59:33 -0700

    move git to parent dir

---
+++ Test/LexerTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestClass]
+        public class ParserTest
+        {
+            [TestMethod]
+            public void MyTest()
+            {
+                Assert.AreEqual(3, 3);
+            }
+        }
+    }
+}
