2feef3717bda8ccba4a005f85f45370c4c127f00
akshaykhot <akshay.7277@gmail.com>
2020-07-03 20:16:36 -0700

    Init Test

---
+++ Test/LexerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+            Assert.AreEqual(3, 3);
+        }
+    }
+}
